59e914f0c40c9cea3bed34c988a3180a67a6296d
Brandon Nay <brandonnay21@gmail.com>
2020-08-16 21:30:35 -0700

    Update testfile.py

--- src/testfile.py
+++ src/testfile.py
@@ -1,28 +1,9 @@
-from mpi4py import MPI
-import platform
-
-
-class Node:
-    class State:
-        def __init__(self, name):
-            self.name = name
-
-    def __init__(self):
-        self.comm = MPI.COMM_WORLD
-        self.rank = self.comm.Get_rank()
-        self.yeet = self.comm.size % (self.rank + 1)
-    
-    def report(self):
-        print(f"HOST:{platform.node()} | S:{self.comm.size} | R:{self.rank} | Y:{self.yeet}")
-
-    def send_receive(self):
-        self.state = self.State(f"we da {self.rank} muzikj")
-        self.states = self.comm.gather(self.state, root=0)
-        
-        print(self.states)
-
-
-if __name__ == "__main__":
-    burger = Node()
-    burger.report()
-    burger.send_receive()
+import os
+print("abc", end="")
+os.system(r"printf '\033[32m'")
+os.system("echo -n 123")
+print("defg", end="")
+os.system(r"printf '\033[39m'")
+os.system("echo 456")
+os.system("echo 789")
+print("hijkl")
